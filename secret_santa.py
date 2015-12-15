76b1d3bbc62248a20689247f81dd4e308d1040b3
Marcos Gabarda <marcosgabarda@gmail.com>
2015-12-15 11:05:49 +0100

    first draft with asyncio

--- secret_santa.py
+++ secret_santa.py
@@ -2,28 +2,33 @@
 Telegram Bot created to deliver secret santa clues and riddles to
 Olga Karavayeva.
 """
+import asyncio
+import sys
+
 import telepot
+import telepot.async
+from telepot.delegate import per_chat_id, create_open
 
 
-class SecretSantaBot(telepot.Bot):
+class SecretSantaBot(telepot.helper.ChatHandler):
 
-    # TOKEN = "163681199:AAHYK1acri1hAUnnLPw1tl2gSON7fAWwfRY"
-    # def __init__(self):
-    #     self.bot = telepot.Bot(TOKEN)
-    #     me = self.bot.getMe()
-    #     for key, value in me.items():
-    #         setattr(self, key, value)
+    def __init__(self, seed_tuple, timeout):
+        super(SecretSantaBot, self).__init__(seed_tuple, timeout)
 
-    def handle(self, msg):
+    @asyncio.coroutine
+    def on_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance2(msg)
         print(content_type, chat_type, chat_id)
-
-    def run(self):
-        self.bot.notifyOnMessage(run_forever=True)
-        print("Listening ...")
-        while True:
-            time.sleep(10)
+        yield from self.sender.sendMessage("cool!")
 
 if __name__ == "__main__":
 
-    secret_santa_bot = SecretSantaBot()
+    TOKEN = sys.argv[1]
+
+    bot = telepot.async.DelegatorBot(TOKEN, [
+        (per_chat_id(), create_open(SecretSantaBot, timeout=10)),
+    ])
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.messageLoop())
+    print('Listening ...')
+    loop.run_forever()

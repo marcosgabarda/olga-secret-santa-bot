733eb704a45e3493a72c7812ff1b06f34e8ed2f4
Marcos Gabarda <marcosgabarda@gmail.com>
2015-12-15 21:16:46 +0100

    almost done!

---
+++ clues.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+CLUES = {
+    "clue-1": {
+        "question": ("text", "Let's start with a standard riddle: 'I have billions of eyes, yet I live in darkness. "
+                             "I have millions of ears, yet only four lobes. I have no muscle, "
+                             "yet I rule two hemispheres. What am I?'\nTry it without using Google ;)"),
+        "solutions": ("a human brain", "brain", "human brain", "brains"),
+        "answers": {
+            "correct": ("text", "Great! You have the first one! :D. "
+                                "Remember, you can use /current to check the new clue ;)"),
+            "wrong": "Ups, that's not the answer you're looking for",
+        },
+        "hints": [
+            "I'm afraid it's quite easy if you use Google",
+            "May be the riddle appear in some comic... may be...",
+            "OK, you can use Google if you are desperate :P",
+        ]
+    },
+    "clue-2": {
+        "question": ("photo", {"caption": "Where was I in this picture?", "file": "files/clue2.png"}),
+        "solutions": ("camino de santiago", "santiago", "cruz de ferro"),
+        "answers": {
+            "correct": ("text", "Great! You have another one! :D "
+                                "Remember, you can use /current to check the new clue ;)"),
+            "wrong": "Ups, that's not the answer you're looking for",
+        },
+        "hints": [
+            "The history of Spain almost start there",
+            "It's like the most famous path!",
+        ]
+    },
+    "clue-3": {
+        "question": ("text", "How does a 'snake' says 'hello' to everyone?"),
+        "solutions": ("print('hello world')", 'print("hello world")', 'print("hello world!")', "print('hello world!')",
+                      'print("hi world")', "print('hi world')"),
+        "answers": {
+            "correct": ("text", "Great! You have one! :D. "
+                                "Remember, you can use /current to check the new clue ;)"),
+            "wrong": "Ups, that's not the answer you're looking for",
+        },
+        "hints": [
+            "Everything is about computers!",
+            "Which is the first program you should learn to write?",
+            "Which is the 'snake' programing language?",
+        ]
+    },
+    "clue-4": {
+        "question": ("audio", {"text": "What am I doing?", "file": "files/clue4.wav"}),
+        "solutions": ("boxing", "rocky", "rocky balboa"),
+        "answers": {
+            "correct": ("text", "Great! You have it! "
+                                "Remember, you can use /current to check the new clue ;)"),
+            "wrong": "Ups, that's not the answer you're looking for",
+        },
+        "hints": [
+            "Never give up!",
+            "I guess what I’m trying to say is, if I can change, and you can change, everybody can change.",
+        ]
+    },
+    "clue-5": {
+        "question": ("photo", {"caption": "I can program this... thing! but, what is ti?", "file": "files/clue5.png"}),
+        "solutions": ("lego", "lego mindstorms", "lego mindstorm", 'mindstorm', 'mindstorms'),
+        "answers": {
+            "correct": ("text", "Great! You have another one! We're almost done! :D "
+                                "Remember, you can use /current to check the new clue ;)"),
+            "wrong": "Ups, that's not the answer you're looking for",
+        },
+        "hints": [
+            "This is not only a 'robotic' stuff, it's also a toy!",
+        ]
+    },
+    "clue-6": {
+        "question": ("video", {"caption": "Who am I going to vote on the next spanish elections?",
+                               "file": "files/clue6.mp4"}),
+        "solutions": ("alberto", "alberto garzon", "alberto garzón", "garzon","garzón"),
+        "answers": {
+            "correct": ("text", "Great! You have another one! You know a lot of things about me now!"
+                                "Remember, you can use /current to check the new clue ;)"),
+            "wrong": "Ups, that's not the answer you're looking for",
+        },
+        "hints": [
+            "They are the cats of the candidate!",
+        ]
+    },
+    "clue-7": {
+        "question": ("text", "Who the hell am I? ;)"),
+        "solutions": ("marcos", "markos", "marcos gabarda", "markos gabarda"),
+        "answers": {
+            "correct": ("sticker", "BQADBAADJgADGolEAAER8FNsi1NergI"),
+            "wrong": "Nope, try again ;)",
+        },
+        "hints": [
+            "No more clues this time :P",
+        ]
+    }
+}
---
+++ main.py
@@ -0,0 +1,175 @@
+"""
+Telegram Bot created to deliver secret santa clues and riddles to
+Olga Karavayeva.
+"""
+import asyncio
+import json
+import logging
+import random
+import sys
+
+import telepot
+from telepot.async.delegate import create_open
+from telepot.delegate import per_chat_id
+
+from clues import CLUES
+
+logging.basicConfig(filename='messages.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class SecretSantaBot(telepot.helper.ChatHandler):
+    """Bot class to handle clues, riddles and answers."""
+
+    help_text = """You can control me by sending these commands:
+
+/answer - give a possible answer to the current clue
+/current - shows the current clue
+"""
+    log = []
+    texts_log = []
+
+    def __init__(self, seed_tuple, timeout):
+        super(SecretSantaBot, self).__init__(seed_tuple, timeout)
+        self.clues = CLUES
+        self._solved_clues = []
+        self._current_command = None
+        self._fails = 0
+        self._set_current_clue(1)
+
+    @staticmethod
+    def _build_message(answer):
+        if isinstance(answer, tuple):
+            return answer
+        return "text", answer
+
+    def _set_current_clue(self, index):
+        self._current_unsolved_clue_index = index
+        self._current_unsolved_clue_key = "clue-%s" % index
+
+    def _get_current_clue(self):
+        return self.clues[self._current_unsolved_clue_key]
+
+    def current_clue_message(self):
+        """Gets the current unsolved question."""
+        try:
+            return self._build_message(self._get_current_clue()["question"])
+        except KeyError:
+            return "text", "Ups, there's no clue right now! Try again later, sorry!"
+
+    def check_answer(self, msg):
+        """Checks the answer given.
+        :param msg:
+        """
+        self._current_command = None
+        clue = self._get_current_clue()
+        message = self._build_message(clue["answers"]["wrong"])
+        kind = message[0]
+        wrong = message[1]
+        content_type, chat_type, chat_id = telepot.glance2(msg)
+        if content_type == "text":
+            answer = msg["text"].strip().lower()
+            if answer in clue["solutions"]:
+                self._fails = 0
+                self._set_current_clue(self._current_unsolved_clue_index + 1)
+                return self._build_message(clue["answers"]["correct"])
+        self._fails += 1
+        if self._fails > random.randint(5, 10):
+            return "text", "Wrong! Some extra clue: %s" % random.choice(clue["hints"])
+        return kind, wrong
+
+    def analyze_message(self, msg):
+        """Analyze commands on the message and gets the proper answers.
+        :param msg:
+        """
+        self.log.append(json.dumps(msg))
+        answer = ("text", "I don't understand what are you saying, sorry!")
+        content_type, chat_type, chat_id = telepot.glance2(msg)
+        if content_type == "text":
+            self.texts_log.append(msg["text"])
+            command = msg['text'].strip().lower().split(' ')[0]
+            # Save current command
+            if command[0] == '/':
+                self._current_command = command
+            # Execute current command
+            if command == '/help':
+                answer = ("text", self.help_text)
+            elif command == "/start":
+                answer = ("text", """Hi Olga!
+Welcome to you Secret Santa Bot! I'll give you some questions/riddles that will give you some clues about myself. \
+Every time you give me a correct answer, I'll give you a new question/riddle, easy!
+
+%s
+""" % self.help_text)
+            elif command == "/current":
+                answer = self.current_clue_message()
+            elif command == "/answer":
+                answer = ("text", "So, what's you answer?")
+            elif command == "/log":
+                answer = ("text", "\n".join(self.log))
+            elif command == "/texts_log":
+                answer = ("text", "\n".join(self.texts_log))
+            elif command[0] != "/":
+                if self._current_command == "/answer":
+                    answer = self.check_answer(msg)
+                else:
+                    answer = ("text", """
+Do you hope I'll give you some extra information for free? Not on my watch!
+
+If you want to give an answer, you should enter /answer command first :)
+""")
+        return answer
+
+    @asyncio.coroutine
+    def open(self, initial_msg, seed):
+        logger.info(json.dumps(initial_msg))
+        full_answer = self.analyze_message(initial_msg)
+        kind = full_answer[0]
+        answer = full_answer[1]
+        if kind == "text":
+            yield from self.sender.sendMessage(answer)
+        elif kind == "photo" and isinstance(answer, dict):
+            with open(answer["file"], "r") as photo_file:
+                yield from self.sender.sendPhoto(photo_file, caption=answer.get("caption", ""))
+        return True
+
+    @asyncio.coroutine
+    def on_message(self, msg):
+        logger.info(json.dumps(msg))
+        full_answer = self.analyze_message(msg)
+        kind = full_answer[0]
+        answer = full_answer[1]
+        if kind == "text":
+            yield from self.sender.sendMessage(answer)
+        elif kind == "photo" and isinstance(answer, dict):
+            with open(answer["file"], "rb") as media_file:
+                yield from self.sender.sendPhoto(media_file, caption=answer.get("caption", ""))
+        elif kind == "video" and isinstance(answer, dict):
+            with open(answer["file"], "rb") as media_file:
+                yield from self.sender.sendVideo(media_file, caption=answer.get("caption", ""))
+        elif kind == "sticker":
+            yield from self.sender.sendSticker(answer)
+        elif kind == "audio" and isinstance(answer, dict):
+            with open(answer["file"], "rb") as media_file:
+                text = answer.get("text", "")
+                if text:
+                    yield from self.sender.sendMessage(text)
+                yield from self.sender.sendAudio(media_file, title=answer.get("text", ""))
+
+    @asyncio.coroutine
+    def on_close(self, exception):
+        if isinstance(exception, telepot.helper.WaitTooLong):
+            yield from self.sender.sendMessage("Good bye!")
+
+
+if __name__ == "__main__":
+    # We get the token from command line...
+    TOKEN = sys.argv[1]
+    # Creates the bot and the event loop...
+    bot = telepot.async.DelegatorBot(TOKEN, [
+        (per_chat_id(), create_open(SecretSantaBot, timeout=604800)),
+    ])
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.messageLoop())
+    print('Listening ...')
+    loop.run_forever()
